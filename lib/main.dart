021d53c1ab42ac93dedbb3e8798d2f1846d976af
Wilson Anyonga <wilsonanyonga@gmail.com>
2021-02-08 06:35:32 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:mwamba_app/constants.dart';
 import 'package:mwamba_app/screens/product/products_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+
+void main() async {
+  
   runApp(MyApp());
 }
 
