b1e782be599f8af67af792b6644fe66de9a89d8c
Wilson Anyonga <wilsonanyonga@gmail.com>
2022-04-17 21:07:15 +0300

    Revert "Update main.dart"
    This reverts commit 0b708b479e3ca03238d692fe064fe383fc20bad5.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,10 @@
 import 'package:flutter/material.dart';
-<<<<<<< Updated upstream
 // import 'package:flutter_downloader/flutter_downloader.dart';
-=======
-import 'package:flutter_downloader/flutter_downloader.dart';
->>>>>>> Stashed changes
 import 'package:mwamba_app/constants.dart';
 import 'package:mwamba_app/screens/product/products_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
-<<<<<<< Updated upstream
 import 'screens/details/components/body.dart';
 
 void main() async {
@@ -17,12 +12,6 @@ void main() async {
     providers: [ChangeNotifierProvider(create: (_) => Counter())],
     child: MyApp(),
   ));
-=======
-
-void main() async {
-  
-  runApp(MyApp());
->>>>>>> Stashed changes
 }
 
 class MyApp extends StatelessWidget {
